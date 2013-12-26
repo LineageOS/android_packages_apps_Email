35a3bb7908f6e15ccae654108c08a752a7b29e0f
yingying <yingying@codeaurora.org>
2013-12-26 22:08:16 +0800

    Email: Let the user could select recipients from Contacts.
     - In the general settings, the user could enable or disable the item
       "select recipients from Contacts". After enable this item, the user
       could select the recipients as to/cc/bcc list in the compose view.
    
    Change-Id: I7529d8b088391daa9a0643923f67651106107478

--- src/com/android/email/activity/setup/GeneralPreferences.java
+++ src/com/android/email/activity/setup/GeneralPreferences.java
@@ -44,6 +44,7 @@ public class GeneralPreferences extends PreferenceFragment implements
     private static final String PREFERENCE_KEY_CONFIRM_SEND = "confirm_send";
     private static final String PREFERENCE_KEY_CONV_LIST_ICON = "conversation_list_icon";
     private static final String PREFERENCE_KEY_ADD_ATTACHMENT = "add_attachment";
+    private static final String PREFERENCE_KEY_SELECT_RECIPIENTS = "select_recipients";
 
     private MailPrefs mMailPrefs;
     private Preferences mPreferences;
@@ -60,6 +61,7 @@ public class GeneralPreferences extends PreferenceFragment implements
     private CheckBoxPreference mSwipeDelete;
 
     private CheckBoxPreference mAddAttachment;
+    private CheckBoxPreference mSelectRecipients;
 
     private boolean mSettingsChanged = false;
 
@@ -140,6 +142,9 @@ public class GeneralPreferences extends PreferenceFragment implements
         } else if (PREFERENCE_KEY_ADD_ATTACHMENT.equals(key)) {
             mPreferences.setAddAttachmentEnabled(mAddAttachment.isChecked());
             return true;
+        } else if (PREFERENCE_KEY_SELECT_RECIPIENTS.equals(key)) {
+            mPreferences.setSelectRecipientsEnabled(mSelectRecipients.isChecked());
+            return true;
         }
         return false;
     }
@@ -177,6 +182,8 @@ public class GeneralPreferences extends PreferenceFragment implements
 
         mAddAttachment = (CheckBoxPreference) findPreference(PREFERENCE_KEY_ADD_ATTACHMENT);
         mAddAttachment.setChecked(mPreferences.getAddAttachmentEnabled());
+        mSelectRecipients = (CheckBoxPreference) findPreference(PREFERENCE_KEY_SELECT_RECIPIENTS);
+        mSelectRecipients.setChecked(mPreferences.getSelectRecipientsEnabled());
 
         reloadDynamicSummaries();
     }
