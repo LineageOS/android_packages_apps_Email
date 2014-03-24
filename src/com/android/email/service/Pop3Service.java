80f3eaa12e9e056e4b899c1a733a007e33e74bbd
yingying <yingying@codeaurora.org>
2014-03-24 15:29:51 +0800

    Email: New a receiver to handle the checking mail action.
    As the recevier need protect the checking mail action by a permission,
    but the receiver also handle the secret code action which couldn't
    protected by the permission. So we need move the checking mail action
    to a new receiver and handle it.
    
    Change-Id: I5e3a5f36ba2512ac8970d3f7650cee16d58d768f

--- src/com/android/email/service/Pop3Service.java
+++ src/com/android/email/service/Pop3Service.java
@@ -67,32 +67,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import android.util.Log;
 
 public class Pop3Service extends Service {
     private static final String TAG = "Pop3Service";
     private static final int DEFAULT_SYNC_COUNT = 100;
-    private static final String ACTION_CHECK_MAIL = "com.android.email.intent.action.MAIL_SERVICE_WAKEUP";
-    private static final String EXTRA_ACCOUNT = "com.android.email.intent.extra.ACCOUNT";
-    private static final String EXTRA_MSGID = "com.android.email.intent.extra.MSGID";
+
+    private static final String ACTION_CHECK_MAIL =
+            "com.android.email.intent.action.MAIL_SERVICE_WAKEUP";
+    private static final String EXTRA_ACCOUNT =
+            "com.android.email.intent.extra.ACCOUNT";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-           Log.d(TAG,"Inside onStartCommand");
-           final String action = intent.getAction();
-           Log.d(TAG,"action is " + action);
-           Context context = getApplicationContext();
-           if (ACTION_CHECK_MAIL.equals(action)) {
-               final long accountId = intent.getLongExtra(EXTRA_ACCOUNT, -1);
-               Log.d(TAG,"accountId is " + accountId);
-               final long inboxId = Mailbox.findMailboxOfType(context, accountId,
-                          Mailbox.TYPE_INBOX);
-               Log.d(TAG,"inboxId is " + inboxId);
-               mBinder.init(context);
-               mBinder.requestSync(inboxId,true,0);
-           }
+        final String action = intent.getAction();
+        LogUtils.d(TAG, "Start with command, action: " + action);
+        Context context = getApplicationContext();
+        if (ACTION_CHECK_MAIL.equals(action)) {
+            final long accountId = intent.getLongExtra(EXTRA_ACCOUNT, -1);
+            final long inboxId = Mailbox.findMailboxOfType(context, accountId,
+                    Mailbox.TYPE_INBOX);
+            LogUtils.d(TAG, "account id is: " + accountId + ", inbox id is: " + inboxId);
+            mBinder.init(context);
+            mBinder.requestSync(inboxId, true, 0);
+        }
 
-           return Service.START_STICKY;
+        return Service.START_STICKY;
     }
 
     /**
