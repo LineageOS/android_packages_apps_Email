7f1a3b1f12f2734746b62a7e4d7da67a703b5f3b
Scott Mertz <scott@cyngn.com>
2015-05-27 17:36:58 -0700

    EmailTests: fix api change
    Change-Id: I170478bb91d847ba9d42515c5d5c29a92f3765dc

--- tests/src/com/android/email/mail/store/ImapStoreUnitTests.java
+++ tests/src/com/android/email/mail/store/ImapStoreUnitTests.java
@@ -1105,7 +1105,7 @@ public class ImapStoreUnitTests extends InstrumentationTestCase {
         MimeUtility.collectParts(message1, viewables, attachments);
         assertTrue(viewables.size() == 1);
         emptyBodyPart = viewables.get(0);
-        String text = MimeUtility.getTextFromPart(emptyBodyPart);
+        String text = MimeUtility.getTextFromPart(emptyBodyPart, true);
         assertNull(text);
     }
 
