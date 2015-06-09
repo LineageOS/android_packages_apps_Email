05ed34b80f91c47cc97732f6bf19141edc40071f
Danny Baumann <dannybaumann@web.de>
2015-06-09 15:30:57 +0200

    Reset idling flag when cancelling early due to being cancelled.
    Change-Id: Idde5f3a3fa04a1aa80ef4cdb9e3ba1ab01d3fcb5

--- provider_src/com/android/email/mail/store/ImapFolder.java
+++ provider_src/com/android/email/mail/store/ImapFolder.java
@@ -252,6 +252,7 @@ public class ImapFolder extends Folder {
                     // the server to listen for new changes)
                     synchronized (mIdleSync) {
                         if (mIdlingCancelled) {
+                            mIdling = false;
                             return;
                         }
                     }
