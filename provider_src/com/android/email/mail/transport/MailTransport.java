ff4fe407afd39aa4767e36e30d8dee11d84fdcdc
Danny Baumann <dannybaumann@web.de>
2015-06-09 14:57:37 +0200

    Assign debug tags to IMAP communication output.
    Allows better tracking of the output if multiple connections are active.
    
    Change-Id: I900c9f9ecda06d3191c4cf82af7e774ae70b7dac

--- provider_src/com/android/email/mail/transport/MailTransport.java
+++ provider_src/com/android/email/mail/transport/MailTransport.java
@@ -55,6 +55,7 @@ public class MailTransport {
             HttpsURLConnection.getDefaultHostnameVerifier();
 
     private final String mDebugLabel;
+    private String mDebugTag;
     private final Context mContext;
     protected final HostAuth mHostAuth;
 
@@ -67,6 +68,10 @@ public class MailTransport {
         mContext = context;
         mDebugLabel = debugLabel;
         mHostAuth = hostAuth;
+    }
+
+    public void setTag(String tag) {
+        mDebugTag = tag;
     }
 
    /**
@@ -292,16 +297,21 @@ public class MailTransport {
         return mOut;
     }
 
+    private String getFormattedDebugTag() {
+        if (mDebugTag != null) {
+            return "(" + mDebugTag + ") ";
+        }
+        return "";
+    }
+
     /**
      * Writes a single line to the server using \r\n termination.
      */
     public void writeLine(String s, String sensitiveReplacement) throws IOException {
         if (DebugUtils.DEBUG) {
-            if (sensitiveReplacement != null && !Logging.DEBUG_SENSITIVE) {
-                LogUtils.d(Logging.LOG_TAG, ">>> " + sensitiveReplacement);
-            } else {
-                LogUtils.d(Logging.LOG_TAG, ">>> " + s);
-            }
+            String output = sensitiveReplacement != null && !Logging.DEBUG_SENSITIVE
+                    ? sensitiveReplacement : s;
+            LogUtils.d(Logging.LOG_TAG, getFormattedDebugTag() + ">>> " + output);
         }
 
         OutputStream out = getOutputStream();
@@ -333,7 +343,7 @@ public class MailTransport {
         }
         String ret = sb.toString();
         if (loggable && DebugUtils.DEBUG) {
-            LogUtils.d(Logging.LOG_TAG, "<<< " + ret);
+            LogUtils.d(Logging.LOG_TAG, getFormattedDebugTag() + "<<< " + ret);
         }
         return ret;
     }
