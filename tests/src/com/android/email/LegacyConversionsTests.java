5670e7303a0b539f3c03aa2ee9eb78342a41359a
Zennoe <agrar.pfurtz@gmail.com>
2017-06-07 23:59:54 +0200

    Part 2 of Adjusts UnitTests to changes made in e353ae2ea19c9ea800d10b37e05de7b0ea1aeeb4
    Change-Id: I1e005a22d1863ce7c5cfde46cc9eea82bc649f2e

--- tests/src/com/android/email/LegacyConversionsTests.java
+++ tests/src/com/android/email/LegacyConversionsTests.java
@@ -388,7 +388,7 @@ public class LegacyConversionsTests extends ProviderTestCase2<EmailProvider> {
         String get1Text = null;
         String get1Html = null;
         for (Part viewable : viewables) {
-            String text = MimeUtility.getTextFromPart(viewable, true);
+            String text = MimeUtility.getTextFromPart(viewable, null);
             if (viewable.getMimeType().equalsIgnoreCase("text/html")) {
                 get1Html = text;
             } else {
